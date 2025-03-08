044c79d1a01f96307d3397cde7f12f011556a615
Matthew Gong <matthew.zhigang.gong@gmail.com>
2025-03-08 23:45:58 +1300

    Make the listing-restaurants e2e test pass

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
+import {Provider} from 'react-redux';
 import RestaurantScreen from './components/RestaurantScreen';
+import store from './store';
 
 function App() {
-  return <RestaurantScreen />;
+  return (
+    <Provider store={store}>
+      <RestaurantScreen />
+    </Provider>
+  );
 }
 
 export default App;
